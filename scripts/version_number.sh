a47d5556a08e7c37b2bbbb005b8ce36a37f8830e
troy <troy@1cb13b44-32ef-483d-9be8-f80555aad9b0>
2009-11-14 23:51:38 +0000

    Fixed versioning in Root.plist, I think

--- scripts/version_number.sh
+++ scripts/version_number.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-REV=`svnversion -n`
-echo TARGET_BUILD_DIR=${TARGET_BUILD_DIR}
 ROOT_PLIST_PATH=${TARGET_BUILD_DIR}/BeerCrush.app/Settings.bundle/Root.plist
-echo ROOT_PLIST_PATH=$ROOT_PLIST_PATH
-/usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:5:DefaultValue $REV" $ROOT_PLIST_PATH
+VER=`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" ${TARGET_BUILD_DIR}/${INFOPLIST_PATH}`
+REV="Version $VER (`svnversion -n`)"
+/usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:4:Title $REV" $ROOT_PLIST_PATH
