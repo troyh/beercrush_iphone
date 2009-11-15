ea573236d4f02e59b6c2eab5c13ef53cc74cf172
troy <troy@1cb13b44-32ef-483d-9be8-f80555aad9b0>
2009-11-15 00:12:16 +0000

    Changed app name to Beer Crush (space between the words)

--- scripts/version_number.sh
+++ scripts/version_number.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-ROOT_PLIST_PATH=${TARGET_BUILD_DIR}/BeerCrush.app/Settings.bundle/Root.plist
-VER=`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" ${TARGET_BUILD_DIR}/${INFOPLIST_PATH}`
+#echo $INFOPLIST_PATH
+#echo $WRAPPER_NAME
+#echo $WRAPPER_SUFFIX
+#echo $WRAPPER_EXTENSION
+#exit 1;
+ROOT_PLIST_PATH=${TARGET_BUILD_DIR}/$WRAPPER_NAME/Settings.bundle/Root.plist
+VER=`/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" "${TARGET_BUILD_DIR}/${INFOPLIST_PATH}"`
 REV="Version $VER (`svnversion -n`)"
-/usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:4:Title $REV" $ROOT_PLIST_PATH
+/usr/libexec/PlistBuddy -c "Set :PreferenceSpecifiers:4:Title $REV" "$ROOT_PLIST_PATH"
